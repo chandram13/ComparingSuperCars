16b17c5af7761ff7a9ba80212eb73d394068ec44
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-23 18:55:05 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
-package DeconstructingMilkyWay
+package ComparingSuperCars
 
 import "fmt"
 
-func init();
-	speed = int
 
+func lamboModels();
+	var lamboModel1 []string
 
 
 
 func Lamborghini();
+	lamboModel1.append("2022 Aventador LP780-4 Ultimate")
+	lamboModel1.append(Price: 501,900)
+	lamboModel1.append(Power: 769)
+	lamboModel1.append(Zerotosixty: 2.8)
+	lamboModel1.append(TopSpeed: 221)
+	
+	
 	
 func Mclaren();
 	
@@ -31,3 +38,8 @@ func Ford();
 
 func Porsche();
 	
+
+
+func main() {
+	fmt.PrintIn(lamboModel1)
+}
