034f0ac46609ede0eff8609b292a921996d6ed86
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-25 16:09:26 -0700

    Add files via upload
    Updated each of the super car models with their 2022 versions.

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import "fmt"
 
 func lamboModels();
 	var lamboModel1 []string
-
+	var lamboModel2 []string 
+	var lamboModel3 []string
+	var lamboModel4 []string
+	var lamboModel5 []string
 
 
 func Lamborghini();
@@ -14,32 +17,207 @@ func Lamborghini();
 	lamboModel1.append(Power: 769)
 	lamboModel1.append(Zerotosixty: 2.8)
 	lamboModel1.append(TopSpeed: 221)
-	
-	
-	
-func Mclaren();
-	
+	lamboModel2.append("2022 Aventdor LP780-4 Ultimate Roadster")
+	lamboModel2.append(Price: 550,000)
+	lamboModel2.append(Power: 769)
+	lamboModel2.append(Zerotosixty: 2.9)
+	lamboModel2.append(TopSpeed: 221)
+	lamboModel3.append("2022 Aventador SVJ")
+	lamboModel3.append(Price: 515,000)
+	lamboModel3.append(Power: 759)
+	lamboModel3.append(Zerotosixty: 2.7)
+	lamboModel3.append(TopSpeed: 217)
+	lamboModel4.append("2022 Aventador SVJ Roadster")
+	lamboModel4.append(Price: 577,000)
+	lamboModel4.append(Power: 759)
+	lamboModel4.append(Zerotosixty: 2.8)
+	lamboModel4.append(TopSpeed: 217)
+	lamboModel5.append("2022 Huracan STO")
+	lamboModel5.append(Price: 331,000)
+	lamboModel5.append(Power: 631)
+	lamboModel5.append(Zerotosixty: 2.8)
+	lamboModel5.append(TopSpeed: 202)
 
+	
+func mclarenModels();
+	var mclarenModel1 []string
+	var mclarenModel2 []string 
+	var mclarenModel3 []string
+	var mclarenModel4 []string
+
+func Mclaren();
+	mclarenModel1.append("2022 720S")
+	mclarenModel1.append(Price: 305,000)
+	mclarenModel1.append(Power: 710)
+	mclarenModel1.append(Zerotosixty: 2.9)
+	mclarenModel1.append(TopSpeed: 212)
+	mclarenModel2.append("720S Spider")
+	mclarenModel2.append(Price: 318,000)
+	mclarenModel2.append(Power: 710)
+	mclarenModel2.append(Zerotosixty: 2.9)
+	mclarenModel2.append(TopSpeed: 212)
+	mclarenModel3.append("765LT")
+	mclarenModel3.append(Price: 368,000)
+	mclarenModel3.append(Power: 755)
+	mclarenModel3.append(Zerotosixty: 2.7)
+	mclarenModel3.append(TopSpeed: 205)
+	mclarenModel4.append("765LT Spider")
+	mclarenModel4.append(Price: 382,500)
+	mclarenModel4.append(Power: 755)
+	mclarenModel4.append(Zerotosixty: 2.7)
+	mclarenModel4.append(TopSpeed: 205)
+
+func ferrariModels();
+	var ferrariModel1 []string
+	var ferrariModel2 []string
+	var ferrariModel3 []string
+	var ferrariModel4 []string
+	var ferrariModel5 []string
+	var ferrariModel6 []string
+	var ferrariModel7 []string
+	var ferrariModel8 []string
 
 func Ferrari();
+	ferrariModel1.append("2022 812 GTS")
+	ferrariModel1.append(Price: 401,500)
+	ferrariModel1.append(Power: 789)
+	ferrariModel1.append(Zerotosixty: 2.8)
+	ferrariModel1.append(TopSpeed: 211)
+	ferrariModel2.append("SF90 Stradale")
+	ferrariModel2.append(Price: 511,295)
+	ferrariModel2.append(Power: 986)
+	ferrariModel2.append(Zerotosixty: 2.5)
+	ferrariModel2.append(TopSpeed: 211)
+	ferrariModel3.append("SF90 Spider")
+	ferrariModel3.append(Price: 570,000)
+	ferrariModel3.append(Power: 989)
+	ferrariModel3.append(Zerotosixty: 2.5)
+	ferrariModel3.append(TopSpeed: 211)
+	ferrariModel4.append("Portofino M")
+	ferrariModel4.append(Price: 227,000)
+	ferrariModel4.append(Power: 612)
+	ferrariModel4.append(Zerotosixty: 3.1)
+	ferrariModel4.append(TopSpeed: 199)
+	ferrariModel5.append("F8 Tributo")
+	ferrariModel5.append(Price: 280,000)
+	ferrariModel5.append(Power: 710)
+	ferrariModel5.append(Zerotosixty: 2.9)
+	ferrariModel5.append(TopSpeed: 211)
+	ferrariModel6.append("F8 Spider")
+	ferrariModel6.append(Price: 302,500)
+	ferrariModel6.append(Power: 710)
+	ferrariModel6.append(Zerotosixty: 2.9)
+	ferrariModel6.append(TopSpeed: 211)
+	ferrariModel7.append("Roma")
+	ferrariModel7.append(Price: 222,620)
+	ferrariModel7.append(Power: 612)
+	ferrariModel7.append(Zerotosixty: 3.2)
+	ferrariModel7.append(TopSpeed: 199)
+	ferrariModel8.append("296 GTB")
+	ferrariModel8.append(Price: 321,400)
+	ferrariModel8.append(Power: 819)
+	ferrariModel8.append(Zerotosixty: 2.8)
+	ferrariModel8.append(TopSpeed: 205)
 	
-
+func bugattiModels();
+	var bugattiModel1 []string
+	var bugattiModel2 []string
+	var bugattiModel3 []string
+	var bugattiModel4 []string
 
 func Bugatti();
-	
+	bugattiModel1.append("2022 Chiron")
+	bugattiModel1.append(Price: 3,300,000)
+	bugattiModel1.append(Power: 1500)
+	bugattiModel1.append(Zerotosixty: 2.4)
+	bugattiModel1.append(TopSpeed: 261)
+	bugattiModel2.append("2022 Chiron Support")
+	bugattiModel2.append(Price: 3,400,000)
+	bugattiModel2.append(Power: 1500)
+	bugattiModel2.append(Zerotosixty: 2.4)
+	bugattiModel2.append(TopSpeed: 261)
+	bugattiModel3.append("2022 Chiron Pur Sport")
+	bugattiModel3.append(Price: 3,600,000)
+	bugattiModel3.append(Power: 1500)
+	bugattiModel3.append(Zerotosixty: 2.4)
+	bugattiModel3.append(TopSpeed: 261)
+	bugattiModel4.append("2022 Chiron Super Sport")
+	bugattiModel4.append(Price: 3,825,000)
+	bugattiModel4.append(Power: 1600)
+	bugattiModel4.append(Zerotosixty: 2.4)
+	bugattiModel4.append(TopSpeed: 273)
+
+func audiModels();
+	var audiModel1 []string
+	var audiModel2 []string
+	var audiModel3 []string
+	var audiModel4 []string	
 
 func Audi();
-	
+	audiModel1.append("R8 Performance Coupe")
+	audiModel1.append(Price: 148,700)
+	audiModel1.append(Power: 562)
+	audiModel1.append(Zerotosixty: 3.7)
+	audiModel1.append(TopSpeed: 3.7)
+	audiModel2.append("R8 Performance Sypder")
+	audiModel2.append(Price: 160,900)
+	audiModel2.append(Power: 562)
+	audiModel2.append(Zerotosixty: 3.8)
+	audiModel2.append(TopSpeed: 203)
+	audiModel3.append("R8 Performance quattro Coupe")
+	audiModel3.append(Price: 199,800)
+	audiModel3.append(Power: 602)
+	audiModel3.append(Zerotosixty: 3.1)
+	audiModel3.append(TopSpeed: 3.1)
+	audiModel4.append("R8 Performance quattro Spyder")
+	audiModel4.append(Price: 213,500)
+	audiModel4.append(Power: 602)
+	audiModel4.append(Zerotosixty: 3.2)
+	audiModel4.append(TopSpeed: 204)
 
+
+func fordModels();
+	var fordModel1 []string
+	var fordModel2 []string
 
 func Ford();
+	fordModel1.append("GT")
+	fordModel1.append(Price: 500,000)
+	fordModel1.append(Power: 647)
+	fordModel1.append(Zerotosixty: 3.0)
+	fordModel1.append(TopSpeed: 216)
+	fordModel2.append("Shelby GT500")
+	fordModel2.append(Power: 760)
+	fordModel2.append(Zerotosixty: 3.3)
+	fordModel2.append(TopSpeed: 180)
+
 	
-
-
-func Porsche();
-	
-
-
 func main() {
 	fmt.PrintIn(lamboModel1)
+	fmt.PrintIn(lamboModel2)
+	fmt.PrintIn(lamboModel3)
+	fmt.PrintIn(lamboModel4)
+	fmt.PrintIn(lamboModel5)
+	fmt.PrintIn(mclarenModel1)
+	fmt.PrintIn(mclarenModel2)
+	fmt.PrintIn(mclarenModel3)
+	fmt.PrintIn(mclarenModel4)
+	fmt.PrintIn(ferrariModel1)
+	fmt.PrintIn(ferrariModel2)
+	fmt.PrintIn(ferrariModel3)
+	fmt.PrintIn(ferrariModel4)
+	fmt.PrintIn(ferrariModel5)
+	fmt.PrintIn(ferrariModel6)
+	fmt.PrintIn(ferrariModel7)
+	fmt.PrintIn(ferrariModel8)
+	fmt.PrintIn(bugattiModel1)
+	fmt.PrintIn(bugattiModel2)
+	fmt.PrintIn(bugattiModel3)
+	fmt.PrintIn(bugattiModel4)
+	fmt.PrintIn(audiModel1)
+	fmt.PrintIn(audiModel2)
+	fmt.PrintIn(audiModel3)
+	fmt.PrintIn(audiModel4)
+	fmt.PrintIn(fordModel1)
+	fmt.PrintIn(fordModel2)
 }
